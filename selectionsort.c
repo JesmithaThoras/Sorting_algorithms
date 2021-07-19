f328092068f74267c03fc98e4abc271909a71793
JesmithaThoras <71216997+JesmithaThoras@users.noreply.github.com>
2021-07-19 20:35:58 +0530

    Create selectionsort.c

---
+++ selectionsort.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void selsort(int a[],int n){
+	int i,temp,j;
+	for(i=0;i<n-1;i++){
+		for(j=i+1;j<n;j++){
+			if(a[j]<a[i]){
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+	for(i=0;i<n;i++){
+		printf("%d ",a[i]);
+	}
+}
+
+int main(){
+	int a[5]={56,34,78,23,1};
+	int n=sizeof(a)/sizeof(a[0]);
+	selsort(a,n);
+	return 0;
+}
